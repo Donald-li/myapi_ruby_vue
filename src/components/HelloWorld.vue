a7fbe8b5cd2d65b6be240fa2be66836b801c9f91
DonaldLee <1026619991@qq.com>
2021-05-08 14:37:19 +0800

    成功与后台进行post数据传递，且完成登陆功能的模拟

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,28 @@
 <template>
   <div>
     <div v-for="(item,index) in users" :key="index">
-      <h1>{{item.name}}</h1>
-      <h1>{{item.password}}</h1>
+      <div>账号ID：{{item.id}} 账号：{{item.name}} 密码：{{item.password}}</div>
     </div>
     <button v-on:click="show">获取</button>
+    <br>
+    <form id="userForm">
+      <label>用户名</label>
+      <input type="text" id="username" v-model="username">
+      <br>
+      <label>密码</label>
+      <input type="password" id="userpassword" v-model="userpassword">
+      <br>
+      <button v-on:click="save">注册</button>
+    </form>
+    <form>
+      <label>用户名</label>
+      <input type="text" v-model="username_log">
+      <br>
+      <label>密码</label>
+      <input type="password" v-model="userpassword_log">
+      <br>
+      <button v-on:click="verification()">登陆</button>
+    </form>
   </div>
 </template>
 
@@ -24,7 +42,11 @@ export default {
           'name': 'donald2',
           'password': '1234'
         }
-      ]
+      ],
+      username:'默认名',
+      userpassword:'123',
+      username_log:'默认名',
+      userpassword_log:'123'
     }
   },
   methods: {
@@ -34,11 +56,44 @@ export default {
         url: '/api/users'
       }).then((response) => {
         this.users = response.data
-        console.log(response)
       }).catch(function (error) {
         console.log(error)
       })
+    },
+    save(){
+      this.axios({
+        method: 'post',
+        url: '/api/users',
+        data: {
+          name: this.username,
+          password: this.userpassword
+        },
+      }).then((response)=>{
+        // this.show()
+      })
+    },
+    //用户登录验证
+    verification(){
+      this.axios({
+        method:"post",
+        url: '/api/users/verification',
+        data:{
+          id: this.username_log,
+          password: this.userpassword_log
+        }
+      }).then((response)=>{
+          if(response.data.state==null) {
+            alert(response.data.name)
+          }else{
+            alert(response.data.msg)
+          }
+      })
     }
+  },
+  mounted () {
+    this.show()
+    //轮询
+    window.setInterval(this.show,5000)
   }
 }
 </script>
